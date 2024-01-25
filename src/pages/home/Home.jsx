1eedef8d58819687c77630fadc8ef4ffd1b2487e
moris96 <moriskhoudari@gmail.com>
2024-01-25 10:53:33 -0500

    cleaned up code in Home.jsx and Home.css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,9 +4,11 @@ import './Home.css';
 const Home = () => {
   return (
     <div className='container-fluid p-0'>
+      {/* text */}
       <h1 className='text-white text-center mt-5'>Step into The Shire's #1 bistro - the coziest Hobbit haven in Middle-Earth.</h1>
       <br/><br/>
       <h1 className='text-white text-center mt-5'>Join us for a culinary journey of joy and simplicity. Your taste buds are in for an unexpected adventure!</h1>
+      
       <div className='background'>
         <div className='jumbotron jumbotron-fluid text-center'>
           <div className='container'>
